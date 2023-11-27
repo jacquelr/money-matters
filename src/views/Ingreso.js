6a9eeb0749b8459fa7f8e906e0c0689f911d0703
jacquelr <a21310437@ceti.mx>
2023-11-27 00:47:23 -0600

    estilos en ingreso

--- src/views/Ingreso.js
+++ src/views/Ingreso.js
@@ -10,18 +10,20 @@ function Ingreso() {
 
       <form>
         <div className="mb-3">
-          <label>Eliga la cuenta a la que se añade este ingreso:</label>
-          <select className="form-select">
+          <label style={{marginTop:'10px'}}>Eliga la cuenta a la que se añade este ingreso:</label>
+          <select className="form-select" style={{marginTop:'10px'}}>
             <option selected>Seleccione una opción</option>
             <option value={1}>Ingreso</option>
             <option value={2}>Gasto</option>
           </select>
 
-          <label>Monto:</label>
-          <input className="form-control" type="number"></input>
+          <label style={{marginTop:'10px'}}>Monto:</label>
+          <input className="form-control" type="number" style={{marginTop:'10px'}} placeholder="$00.00"></input>
 
-          <label>Fecha:</label>
-          <input type="date"></input>
+          <label style={{marginTop:'10px'}}>Fecha:</label>
+          <input className="form-control" type="date" style={{marginTop:'10px'}}></input>
+
+          <button className="btn btn-primary" style={{marginTop:'20px'}}>Agregar</button>
         </div>
       </form>
     </div>
