14a5827d49e84c79586436953435b78a86f9c056
jacquelr <a21310437@ceti.mx>
2023-11-27 21:47:44 -0600

    modificacion inicio

--- src/views/Inicio.js
+++ src/views/Inicio.js
@@ -37,12 +37,29 @@ function Inicio() {
   return (
     
     <div>
-        <Sidebar />
-        <br/><br/><br/><br/>
-        <div className="pie-chart">
-          <Pie data={data} options={options} style={{width: '600px', height: '400px', position: 'relative', top: '50px', left: '-300px'}}/>
+      <Sidebar />
+      <div className='container' style={{marginTop:'110px'}}>
+        <div className='row'>
+          <div className='col-4'>
+            <div className='shadow p-3 mb-5 bg-white rounded' style={{height:'330px'}}>
+              <div className="pie-chart" style={{height:'300px'}}>
+                <Pie data={data} options={options}/>
+              </div>
+            </div>
+          </div>
+          <div className='col-8'>
+          <div className='shadow-lg p-4 mt-4 mb-5 bg-white rounded'>
+              <h2> B I E N V E N I D O </h2>
+            </div>
+            <div className='shadow-lg p-2 mb-3 bg-white rounded'>
+              <h3> Total de ingresos: </h3>
+            </div>
+            <div className='shadow-lg p-2 mb-3 bg-white rounded'>
+            <h3> Total de gastos: </h3>
+            </div>
+          </div>
         </div>
-
+      </div>
     </div>
   )
 }
